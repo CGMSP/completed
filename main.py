56339fce28b31e0eadfe1387a66f2e055ff53e0e
Kaz Malhotra <kazmal@protonmail.com>
2020-02-18 13:28:24 -0800

    blah

---
+++ main.py
@@ -0,0 +1,27 @@
+# print
+print("Stuff")
+
+# Variables
+x = 'stuff'
+print(x)
+
+# lists
+fruits = ['apple', 'banana', 'orange']
+print("The 0th entry in fruits is" fruits[0])
+print("fruits is", fruits)
+fruits.append('pomagranate')
+print("fruits in now" fruits)
+
+# Conditional statements
+myBool = True
+if myBool == true:
+  print("My boolean variable is true")
+elif myBool == false:
+  print("My boolean variable is false.")
+else:
+  print("Oops")
+
+# Input
+name = input("What is your name?")
+age = float(input("How old are you"))
+info = [name, age]
